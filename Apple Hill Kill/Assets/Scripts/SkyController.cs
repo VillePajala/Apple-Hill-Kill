c6ee0c38e42122207f901b388bc0b17ec2d70280
VillePajala <valoraami@gmail.com>
2018-09-27 13:38:01 +0300

    Added physic and lighting

---
+++ Apple Hill Kill/Assets/Scripts/SkyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour {
+
+    public float rotate = 0f;
+
+	void Start () {
+		
+	} // Start
+	
+
+	void Update () {
+
+        this.rotate += Time.deltaTime;
+        RenderSettings.skybox.SetFloat("_Rotation", this.rotate);
+		
+	} // Update
+}
