11abe4d7bafdca623f09c2f3c182642d7baa265f
VillePajala <valoraami@gmail.com>
2018-10-09 19:55:46 +0300

    Added screenshots

---
+++ Apple Hill Kill/Assets/Scripts/CloseGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGame : MonoBehaviour {
+
+	
+	void Start () {
+		
+	} // Start
+	
+	
+	void Update () {
+		
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            // Closing the Game
+            Application.Quit();
+        }
+
+	} // Update
+} // Class
